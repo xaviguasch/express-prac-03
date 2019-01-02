6bff88b7ece21a58416860b89232d738ab7d681b
xaviguasch <xaviguasch@gmail.com>
2019-01-02 15:00:35 +0100

    adding bodyparser and redirecting

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
+
+const friends = ['Tony', 'Miranda', 'Justin', 'Pierre', 'Lily']
+
 
 app.get('/', function(req, res) {
     res.render('home')
 })
 
 app.get('/friends', function(req, res) {
-    const friends = ['Tony', 'Miranda', 'Justin', 'Pierre', 'Lily']
     res.render('friends', {friends: friends})
 })
 
 app.post('/addfriend', function(req, res) {
-    res.send('YOU HAVE REACHED THE POST ROUTE!!!')
+    const newFriend = req.body.newfriend
+
+    friends.push(newFriend)
+    
+    res.redirect('friends')
 })
 
 
